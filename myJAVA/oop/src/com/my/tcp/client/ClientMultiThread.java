94ef9a42a76be73eba9da3c16aea0b2ecb09dfdc
chandoll <dhcksehf@gmail.com>
2023-08-10 15:57:31 +0900

    08-10 Client 멀티스레드

--- myJAVA/oop/src/com/my/tcp/client/ClientMultiThread.java
+++ myJAVA/oop/src/com/my/tcp/client/ClientMultiThread.java
@@ -2,6 +2,7 @@ package com.my.tcp.client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -11,7 +12,47 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class ClientTest {
+class ClientThread extends Thread {
+	// 소켓을 통해서 데이터를 수신받는 역할
+	
+	private Socket s;
+	
+	InputStream is =null;
+	DataInputStream dis = null;
+	
+	//생성자
+	ClientThread(Socket s) throws IOException {
+		this.s = s;
+		is = s.getInputStream();
+		dis = new DataInputStream(is);
+	}
+	
+	@Override
+	public void run() {
+		
+		try {
+			
+			while(true) {
+				String receiveMsg = dis.readUTF();
+				System.out.println("서버가 되돌려준 메시지: " + receiveMsg);
+			} // while
+			
+		} catch (SocketException e) {
+		} catch (EOFException e) {
+		} catch (IOException e) {
+			e.printStackTrace();
+		} //try-catch
+		
+	} // run()
+	
+	
+} // ClientThread
+
+class send extends Thread {
+	
+}
+
+public class ClientMultiThread {
 
 	public static void main(String[] args) {
 		
@@ -23,39 +64,27 @@ public class ClientTest {
 		
 		OutputStream oos = null;
 		DataOutputStream dos = null;
-		Scanner sc = new Scanner(System.in);
 		
-		InputStream is =null;
-		DataInputStream dis = null;
+		Scanner sc = new Scanner(System.in);
 		
 		try {
 			
 			s = new Socket(serverIp, serverPort);
 			System.out.println("서버와 연결 성공");
 			
+			// 새로운 스레드 시작
+			new ClientThread(s).start(); // 소켓이 새로운 스레드와 메인 스레드의 공유인자
+			
 			// 서버로 데이터 보내기
 			oos = s.getOutputStream();
-			
-//			oos.write(66);
-			
 			dos = new DataOutputStream(oos);
-//			dos.writeUTF("셍나는 바보 ㅡㅅ ㅡ");
-			
-			is = s.getInputStream();
-			dis = new DataInputStream(is);
 			
 			String sendMsg;
 			do {
 				System.out.println("서버로 보낼 메시지(종료하려면 quit를 입력하세요.) : ");
 				sendMsg = sc.nextLine();
 				dos.writeUTF(sendMsg);
-				
-				String receiveMsg = dis.readUTF();
-				System.out.println("서버로부터 받은 메세지: " + receiveMsg);
-
 			} while(!sendMsg.equals("quit"));
-			
-			// 서버로부터 데이터 받기
 			
 		} catch (UnknownHostException e) {
 			System.out.println(serverIp + "서버가 존재하지 않습니다. 서버 IP를 확인하세요 -ㅅ- ");
@@ -75,8 +104,6 @@ public class ClientTest {
 			} // if
 		} // try-catch-finally
 		
-		// 192.168.1.22 학원 아이피
-
 	} // main
 
 } // end class
--- myJAVA/oop/src/com/my/tcp/server/ServerTest.java
+++ myJAVA/oop/src/com/my/tcp/server/ServerTest.java
@@ -1,9 +1,11 @@
 package com.my.tcp.server;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,6 +22,9 @@ public class ServerTest {
 		InputStream is = null;
 		DataInputStream dis = null;
 		
+		OutputStream oos = null;
+		DataOutputStream dos = null;
+		
 		try {
 			
 			ss = new ServerSocket(port);
@@ -33,11 +38,22 @@ public class ServerTest {
 			
 			dis = new DataInputStream(is);
 			
+			oos = s.getOutputStream();
+			dos = new DataOutputStream(oos);
+			
 			String receiveMsg;
 			
 			while( !(receiveMsg = dis.readUTF()).equals("quit") ) {				
 				System.out.println("클라이언트가 보낸 메시지: " + receiveMsg);
+			
+				dos.writeUTF(receiveMsg);
+				System.out.println("클라이언트로 메세지를 보냈습니다.");
 			}
+			
+		
+			
+			// ==================== 클라이언트로 메세지 보내기 ================
+			
 			
 		} catch (EOFException e) {
 		
