c2794080de1b3f1e21f95f27cf48b1b45b233c4d
chandoll <dhcksehf@gmail.com>
2023-08-30 14:04:52 +0900

    html03, css

---
+++ myJAVA/back/src/main/java/control/FormTestController.java
@@ -0,0 +1,23 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form")
+public class FormTestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("doGet() 호출되었습니다. :)");
+	} // doGet
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("doPost() 호출되었습니다. :)");
+
+	} // doPost
+
+} // endclass
