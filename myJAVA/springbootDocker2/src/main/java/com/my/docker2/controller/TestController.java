f5e8156e6fa124b130078a98dc0120e8f92171ba
ohchanseok <dhcksehf@gmail.com>
2023-11-06 16:58:22 +0900

    11월6일 Docker,MSA 공부

---
+++ myJAVA/springbootDocker2/src/main/java/com/my/docker2/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.my.docker2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.my.docker2.dao.DockerARepository;
+import com.my.docker2.entity.DockerA;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	private DockerARepository repository;
+	
+	
+	@GetMapping("/a")
+	@ResponseBody
+	public String a() {
+		
+		DockerA entity = DockerA.builder().a1("a1_1").a2("a2_1").build();
+		
+		repository.save(entity);
+		
+		// entity가 잘 만들어졌는지 리턴해보자
+		return entity.toString();
+		
+	} // a()
+	
+}
--- myJAVA/springboot_boardJPA/src/main/java/com/my/board/dao/BoardRepository.java
+++ myJAVA/springboot_boardJPA/src/main/java/com/my/board/dao/BoardRepository.java
@@ -1,12 +1,24 @@
 package com.my.board.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.my.board.entity.BoardEntity;
-import com.my.board.entity.ReplyEntity;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer>{
 
+	// 게시글 상세 검색용 메서드, JPQL사용
+//	@Query(native=true, "SELECT *\r\n"
+//			+ "		FROM board b LEFT JOIN \r\n"
+//			+ "			(SELECT level,r1.* FROM board_reply r1 START WITH reply_parent_no IS NULL CONNECT BY PRIOR reply_no =  reply_parent_no \r\n"
+//			+ "			 ORDER SIBLINGS BY reply_no DESC)r\r\n"
+//			+ "		ON b.board_no = r.reply_board_no\r\n"
+//			+ "		WHERE board_no = #{boardNo}")
+//	public List<Object[]> findByBoardNo(int boardNo);
+	// 리턴되는 타입이 entity가 아닌 경우 List<Object[]> 배열타입으로 해야 함
+	
 //	public void saveReply(Integer boardNo, ReplyEntity r);
 
 //	public BoardEntity findA(BoardEntity b);
