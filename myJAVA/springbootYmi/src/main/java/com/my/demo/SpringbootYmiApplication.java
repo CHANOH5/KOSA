a30d562034c85f19fb0b5ce3bd3aa532aab76f37
ohchanseok <dhcksehf@gmail.com>
2023-11-07 17:01:01 +0900

    11월7일 Docker,MSA

---
+++ myJAVA/springbootYmi/src/main/java/com/my/demo/SpringbootYmiApplication.java
@@ -0,0 +1,13 @@
+package com.my.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootYmiApplication.class, args);
+	}
+
+}
