2e5bed746aed8ea191f5a26ac4d2ac4bb5b7efef
ohchanseok <dhcksehf@gmail.com>
2023-11-10 16:57:55 +0900

    vue.js 프로젝트 생성 및 실습 + 이벤트처리 공부1

--- myVue3.2/test-vue-app1/src/components/Checkbox1.vue
+++ myVue3.2/test-vue-app1/src/components/Checkbox1.vue
@@ -2,6 +2,10 @@
     <div>
         <!-- <li><input type="checkbox" v-bind:checked="chk"/>{{name}}</li> -->
         <li><input type="checkbox" v-bind:checked="p.checked"/>{{p.prodName}}</li>
+        <!-- 객체 자료형이 전달될 때에는 양방향으로 전달 가능하다 -->
+        <!-- 메모리 주소를 바꾸려는 시도가 아니라 객체 값을 변경하려는 시도이기 때문 -->
+        <!-- 하지만 이렇게 하지마라!! -->
+        <!-- <input type="text" v-model="p.prodName"> -->
     </div>
 </template>
 <script>
--- myVue3.2/test-vue-app1/src/main.js
+++ myVue3.2/test-vue-app1/src/main.js
@@ -1,7 +1,9 @@
 import './assets/main.css'
 
+// root component 설정
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from './App1.vue'
+// import App from './App1.vue'
+import App from './App2.vue'
 
 createApp(App).mount('#app')
